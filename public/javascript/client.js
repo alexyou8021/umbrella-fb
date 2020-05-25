36dc4440186aed692d6a7394597472c9a6fc8647
Alexander You <alex@samuel-mbp.local>
2020-05-25 04:49:12 -0500

    list transactions as items

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,9 +1,21 @@
 function myFunction() {
   const HTTP = new XMLHttpRequest();
   const url = "https://sleeper-wrapper.herokuapp.com/user/" + document.getElementById("fname").value;
+  HTTP.responseType = 'json';
   HTTP.open("GET", url);
   HTTP.send();
   HTTP.onreadystatechange=(e)=>{
-    document.getElementById("demo").innerHTML = HTTP.responseText;
+    var resp = HTTP.response;
+    document.getElementById("demo").innerHTML = "";
+    var transactions = document.getElementById("transaction_list")
+    if (resp != null) {
+      for(var i = 0; i < resp.length; i++) {
+        var obj = resp[i];
+        var li = document.createElement("transaction" + i.toString());
+        li.innerHTML = JSON.stringify(obj) + "<br>";
+        transactions.appendChild(li);
+        console.log(obj);
+      }
+    }
   }
 }
