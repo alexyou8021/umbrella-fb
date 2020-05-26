a7f38fd635a25ac777ae73a54eb457bc42908728
Alexander You <alex@samuel-mbp.local>
2020-05-26 02:52:36 -0500

    updated frontend look

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -1,6 +1,6 @@
 function myFunction() {
   const HTTP = new XMLHttpRequest();
-  const url = "https://sleeper-wrapper.herokuapp.com/user/" + document.getElementById("fname").value;
+  const url = "https://sleeper-wrapper.herokuapp.com/user/" + document.getElementById("search").value;
   HTTP.responseType = 'json';
   HTTP.open("GET", url);
   HTTP.send();
@@ -8,7 +8,6 @@ function myFunction() {
     var resp = HTTP.response;
     var transactions = document.getElementById("transaction_list")
     transactions.innerHTML = "";
-    document.getElementById("demo").innerHTML = "";
     if (resp != null) {
       var previousWeek = -1;
       var weekRow = document.createElement("div");
@@ -26,12 +25,21 @@ function myFunction() {
       			weekRow = document.createElement("div");
       			weekRow.className = "row";
 		}
-		weekRow.textContent = "Week " + week;
+      		var weekText = document.createElement("div");
+		weekText.textContent = "Week " + week;
+      		weekText.className = "row2";
+		weekText.style.fontWeight = "bold";
+      		weekText.style.backgroundColor = "gray";
+		weekRow.appendChild(weekText);
 		previousWeek = week;
+	}
+	else {
+		var divider = document.createElement("hr");
+		weekRow.appendChild(divider);
 	}
 
         var transactionRow = document.createElement("div");
-	transactionRow.className = "row"
+	transactionRow.className = "col-container"
 	var addCol = document.createElement("div");
 	addCol.className = "column"
 	for (var key in adds) {
@@ -39,6 +47,7 @@ function myFunction() {
 		if (isNaN(id)) {
 			id = id.toLowerCase()
 			var image = document.createElement("IMG");
+			image.className = "reduced";
 			image.setAttribute("src", "https://sleepercdn.com/images/team_logos/nfl/" + id + ".png");
 			addCol.appendChild(image);
 		}
@@ -47,15 +56,23 @@ function myFunction() {
 			image.src = "https://sleepercdn.com/content/nfl/players/" + id + ".jpg";
 			addCol.appendChild(image);
 		}
+
+		var desc = document.createElement("div");
+		desc.className = "row"
+		desc.textContent = "Added";
+		addCol.appendChild(desc);
+
 		var name = document.createElement("div");
 		name.className = "row"
+		name.style.fontWeight = "bold";
 		name.textContent = key;
 		addCol.appendChild(name);
 	}
 	transactionRow.appendChild(addCol);
 
 	var scoreCol = document.createElement("div");
-	scoreCol.className = "column";
+	scoreCol.className = "column2";
+	scoreCol.style.fontWeight = "bold";
 	scoreCol.textContent = score;
 	transactionRow.appendChild(scoreCol);
 
@@ -66,6 +83,7 @@ function myFunction() {
 		if (isNaN(id)) {
 			id = id.toLowerCase()
 			var image = document.createElement("IMG");
+			image.className = "reduced";
 			image.setAttribute("src", "https://sleepercdn.com/images/team_logos/nfl/" + id + ".png");
 			dropCol.appendChild(image);
 		}
@@ -74,8 +92,15 @@ function myFunction() {
 			image.src = "https://sleepercdn.com/content/nfl/players/" + id + ".jpg";
 			dropCol.appendChild(image);
 		}
+
+		var desc = document.createElement("div");
+		desc.className = "row"
+		desc.textContent = "Dropped";
+		dropCol.appendChild(desc);
+
 		var name = document.createElement("div");
 		name.className = "row"
+		name.style.fontWeight = "bold";
 		name.textContent = key;
 		dropCol.appendChild(name);
 	}
